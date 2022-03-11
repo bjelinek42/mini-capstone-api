840c8b4dacff5045afb7ca1e8bc7c3866c1d65ef
ben <bjelinek42@gmail.com>
2022-03-11 11:50:28 -0700

    index/show/post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product
+  end
+
+  def index
+    products = Product.all
+    render json: products
+  end
+
+  def create
+    product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_image_url],
+      description: params[:input_description])
+    product.save
+    render json: product
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
+
+  post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
 end
