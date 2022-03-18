a257f475498fc7c32e0802f8f5d07da79c1d8cf5
ben <bjelinek42@gmail.com>
2022-03-18 10:51:17 -0600

    fix controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,11 +27,11 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:input_name]
-    @product.price = params[:input_price]
-    @product.image_url = params[:input_url]
-    @product.description = params[:input_description]
-
+    @product.name = params[:input_name] || @product.name
+    @product.price = params[:input_price] || @product.price
+    @product.image_url = params[:input_url] || @product.image_url
+    @product.description = params[:input_description] || @product.description
+    @product.supplier_id = params[:input_supplier_id] || @product.supplier_id
     if @product.save
       render template: "products/show"
     else
