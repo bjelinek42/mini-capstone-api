9d8f8f01623db8a4524fcb67e2362a76fad18b31
ben <bjelinek42@gmail.com>
2022-04-20 11:32:44 -0600

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,75 @@
-# Product.create!([
-#   {name: "pizza", price: "15.0", image_url: "https://www.simplyrecipes.com/thmb/8caxM88NgxZjz-T2aeRW3xjhzBg=/2000x1125/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-8f256746d649404baa36a44d271329bc.jpg", description: "cheese, bread, sauce", inventory: nil},
-#   {name: "salad", price: "10.0", image_url: "https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad-500x375.jpg", description: "leaves", inventory: nil},
-#   {name: "shoes", price: "45.0", image_url: "https://assets.adidas.com/images/w_600,f_auto,q_auto/4e894c2b76dd4c8e9013aafc016047af_9366/Superstar_Shoes_White_FV3284_01_standard.jpg", description: "foot gloves", inventory: nil},
-#   {name: "gloves", price: "35.0", image_url: "https://cdn.shopify.com/s/files/1/1249/3023/products/FineTOUGHOKGloveIndigoWhite.png?v=1637343200", description: "hand shoes", inventory: nil},
-#   {name: "shirt", price: "15.0", image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSLvktLs1ESMN7QNVvTulcwvss5sq3_jBgNfo_FOoZoCvelF1v-h1cY1zoZ2i6pa1nIBpzpdyc1c7ADc9N_mJx8k3JsGcnoaSwIHQGk_9CkHNIre8lmsVk2&usqp=CAE", description: "arm pants", inventory: nil},
-#   {name: "shirt", price: "50.0", image_url: nil, description: nil, inventory: nil},
-#   {name: nil, price: nil, image_url: nil, description: nil, inventory: nil},
-#   {name: nil, price: nil, image_url: nil, description: nil, inventory: nil},
-#   {name: "gloves", price: "35.0", image_url: "https://cdn.shopify.com/s/files/1/1249/3023/products/FineTOUGHOKGloveIndigoWhite.png?v=1637343200", description: "hand shoes", inventory: nil}
-# ])
+User.create!([
+  {name: "Test name", email: "test@test.com", password_digest: "$2a$12$222xhfZu6azMXq/4teKere9bR9Mt8xL5DRppzlVMYenlQVzCBxnb2", admin: false},
+  {name: "Ben", email: "ben@test.com", password_digest: "$2a$12$FJEx/uPBWjmaO3p94oqXvueP5WkPeH.ZZ7Ro/CeMEd2LeIOXubc0S", admin: true}
+])
+Image.create!([
+  {url: "https://images.squarespace-cdn.com/content/v1/5eb2f5532d212a0e2b3f2102/1600972148147-KKI2J85MHO7WLFD8YOF9/Per+Location_Pizza+Slice+Cheese+Pull.jpg?format=1500w", product_id: 1},
+  {url: "https://3dur7j6ifxs3y5qo2e7mraga-wpengine.netdna-ssl.com/wp-content/uploads/2020/02/Leon-Gessi-2.jpg", product_id: 1},
+  {url: "https://3dur7j6ifxs3y5qo2e7mraga-wpengine.netdna-ssl.com/wp-content/uploads/2020/02/Rufranos-1.jpg", product_id: 1},
+  {url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bestsweatshirtswomen-1619798167.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*", product_id: 16}
+])
+Category.create!([
+  {name: "food"},
+  {name: "clothes"}
+])
+Supplier.create!([
+  {name: "Foods 'R' Us", email: "foods@foodsrus.com", phone_number: "890-432-4356"},
+  {name: "Shirts 'n' Stuff", email: "stuff@shirtsnstuff.com", phone_number: "890-654-4426"},
+  {name: nil, email: nil, phone_number: nil}
+])
+CartedProduct.create!([
+  {user_id: 1, product_id: 9, quantity: 2, status: "removed", order_id: nil},
+  {user_id: 1, product_id: 9, quantity: 2, status: "removed", order_id: nil},
+  {user_id: 1, product_id: 10, quantity: 2, status: "purchased", order_id: 1},
+  {user_id: 1, product_id: 9, quantity: 2, status: "purchased", order_id: nil},
+  {user_id: 1, product_id: 9, quantity: 2, status: "purchased", order_id: nil},
+  {user_id: 1, product_id: 9, quantity: 2, status: "purchased", order_id: nil},
+  {user_id: 2, product_id: 9, quantity: 2, status: "removed", order_id: nil},
+  {user_id: 2, product_id: 10, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 10, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 10, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 10, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 10, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 9, quantity: 2, status: "purchased", order_id: 13},
+  {user_id: 2, product_id: 9, quantity: 2, status: "removed", order_id: nil}
+])
+Product.create!([
+  {name: "salad", price: "10.0", description: "leaves and other stuff", inventory: nil, supplier_id: 1},
+  {name: "cake", price: "23.0", description: "delicious cake", inventory: nil, supplier_id: 1},
+  {name: "pants", price: "35.0", description: "leg shirts", inventory: nil, supplier_id: 2},
+  {name: "shirt", price: "15.0", description: "arm pants duh", inventory: nil, supplier_id: 2},
+  {name: "gloves", price: "35.0", description: "hand shoes", inventory: nil, supplier_id: 2},
+  {name: "shoes", price: "45.0", description: "foot gloves", inventory: nil, supplier_id: 2},
+  {name: "Hat", price: "23.0", description: "fattening delicious cake", inventory: nil, supplier_id: 2},
+  {name: "Sweat Pants", price: "40.0", description: "makes legs sweaty", inventory: nil, supplier_id: 2},
+  {name: "sweat pants", price: "40.0", description: "makes legs sweaty", inventory: nil, supplier_id: 2},
+  {name: "sweat pants", price: "40.0", description: "makes legs sweaty", inventory: nil, supplier_id: 2},
+  {name: "sweatshirt", price: "35.0", description: "makes torso sweaty", inventory: nil, supplier_id: 2},
+  {name: "sweatshirt", price: "35.0", description: "makes torso sweaty", inventory: nil, supplier_id: 2}
+])
+CategoryProduct.create!([
+  {product_id: 2, category_id: 1},
+  {product_id: 10, category_id: 1},
+  {product_id: 3, category_id: 2},
+  {product_id: 4, category_id: 2},
+  {product_id: 5, category_id: 2},
+  {product_id: 9, category_id: 2}
+])
+Order.create!([
+  {user_id: 1, subtotal: "30.0", tax: "2.7", total: "32.7"},
+  {user_id: 1, subtotal: "30.0", tax: "2.7", total: "32.7"},
+  {user_id: 1, subtotal: "30.0", tax: "2.7", total: "32.7"},
+  {user_id: 2, subtotal: "20.0", tax: "1.8", total: "21.8"},
+  {user_id: 1, subtotal: "256.0", tax: "23.04", total: "279.04"},
+  {user_id: 1, subtotal: "256.0", tax: "23.04", total: "279.04"},
+  {user_id: 1, subtotal: "256.0", tax: "23.04", total: "279.04"},
+  {user_id: 1, subtotal: "256.0", tax: "23.04", total: "279.04"},
+  {user_id: 1, subtotal: "0.0", tax: "0.0", total: "0.0"},
+  {user_id: 1, subtotal: "0.0", tax: "0.0", total: "0.0"},
+  {user_id: 1, subtotal: "0.0", tax: "0.0", total: "0.0"},
+  {user_id: 2, subtotal: "300.0", tax: "27.0", total: "327.0"},
+  {user_id: 2, subtotal: "0.0", tax: "0.0", total: "0.0"},
+  {user_id: 2, subtotal: "0.0", tax: "0.0", total: "0.0"},
+  {user_id: 2, subtotal: "0.0", tax: "0.0", total: "0.0"}
+])
 
-supplier = Supplier.new({name: "Foods 'R' Us", phone_number: "890-432-4356", email: "foods@foodsrus.com"})
-supplier.save
-
-supplier = Supplier.new({name: "Shirts 'n' Stuff", phone_number: "890-654-4426", email: "stuff@shirtsnstuff.com"})
-supplier.save
